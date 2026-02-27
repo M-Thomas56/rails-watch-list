133d95a8a96f1374cd5b5bcc298002d0fa261922
marc thomas <marc.thomas5608@gmail.com>
2026-02-27 12:46:38 +0100

    coucou perruche

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,20 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "Cleaning the db"
+Movie.destroy_all
+List.destroy_all
+puts "creating the new db"
+Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman entre en conflit avec l'Union soviétique pendant la Guerre Froide dans les années 1980 !",
+poster_url: "https://fr.web.img4.acsta.net/pictures/20/12/07/17/38/1866668.jpg",
+rating: 6.9)
+Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman entre en conflit avec l'Union soviétique pendant la Guerre Froide dans les années 1980 !",
+poster_url: "https://fr.web.img4.acsta.net/pictures/20/12/07/17/38/1866668.jpg",
+rating: 6.9)
+Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
+Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
+Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
+puts "movies created with success"
+
+List.create(name: "Drama")
+List.create(name: "Comedy")
+List.create(name: "Classic")
+List.create(name: "To rewatch")
+puts "lists created with success"
